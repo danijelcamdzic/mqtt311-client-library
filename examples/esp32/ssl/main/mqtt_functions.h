3ce31b521b66b8973aa323414c5635192977041a
danijelcamdzic <danijelcamdzic@gmail.com>
2023-07-29 18:29:19 +0200

    Fix examples since MQTT broker was changed

--- examples/esp32/ssl/main/mqtt_functions.h
+++ examples/esp32/ssl/main/mqtt_functions.h
@@ -20,7 +20,8 @@
 #include "esp_log.h"
 
 /* MQTT connection specific defines */
-#define BROKER_ADDRESS              "mqtt.eclipseprojects.io"
+#define BROKER_ADDRESS              "test.mosquitto.org"
+#define BROKER_PORT_TCP             1883
 #define BROKER_PORT_SSL             8883
 #define KEEP_ALIVE                  600
 #define CLIENT_ID                   "client_id"
